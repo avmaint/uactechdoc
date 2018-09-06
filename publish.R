abd87bd966c6c9e7d65b583d04e948d7aa9c09d7
avmaint <avmaint@uachome.org>
2018-09-06 07:13:49 -0400

    working on code for PDF conversion

--- publish.R
+++ publish.R
@@ -10,15 +10,16 @@ files <- c(
   "SystemDesignLighting" ,
   "SystemDesignNetwork",
   "SystemDesignProjection",
+  "SystemDesignVideo",
 
   "SystemOperationsAudio",
   "SystemOperationsLighting",
   "SystemOperationsProjection",
   "SystemOperationsVideo",
-  
+
   "TechHomePage",
-  
-  "TeamDesign", 
+
+  "TeamDesign",
   "ElectricalGuide",
           
   "index"
@@ -46,5 +47,26 @@ for (f in files) {
   
   system(cmd)
 }
+
+chromecmd <- "/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome"
+chromeopt <- "--headless --disable-gpu --print-to-pdf "
+
+for (f in files) {
+
+  sfile <- paste0(trgdir, "/", f, ".html"  )
+
+  cmd <- paste(chromecmd, chromeopt, sfile)
+  print(cmd)
+  #system(cmd)
+}
   
 print("Complete.")
+
+# TODO Explore ways to also produce PDF
+#  Option 1  - use chrome headless for the conversion
+# alias chromeX="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome" 
+# /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --disable-gpu --print-to-pdf  file:///Users/donert/Dropbox/UAC_Tech_Docs/SystemOperationsVideo.html
+# chromeX 
+
+#TODO: Add change log report to each document:
+#git log --date=local --pretty=format:"%h%x09%an%x09%ad%x09%s" -- SystemOperationsLighting.Rmd
