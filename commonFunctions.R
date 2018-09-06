ec8e437d08ee067d39188a73e7ce553e0c565587
avmaint <avmaint@uachome.org>
2018-09-06 07:38:44 -0400

    Added Change Log feature

--- commonFunctions.R
+++ commonFunctions.R
@@ -19,3 +19,28 @@ print_inv <- function(items, inventory) {
     column_spec( 1,  bold = TRUE ) %>%
     kable_styling("striped", full_width = TRUE)
 }
+
+#Function to retrieve and format the git commit history for a file. 
+#Meant to tack on to the end of the each document.
+#takes a file name, returns html formatted table
+#filename meant to be provided 
+#intended usage: commit.log.html( knitr::current_input() )
+
+commit.log.html <- function(file.name) {
+  
+  cmd <- "git"
+  opt <- 'log --date=local --pretty=format:"%h,%an,%ad,%s" --'
+  
+  args <- paste( opt, file.name)
+  
+  o <- system2(cmd, args=args, stdout=TRUE)
+  
+  df <- read.csv(text=o, col.names=c("id", "user", "date", "message"))
+  
+  return( df %>% 
+            select(user, date, message) %>%
+            kable(align="l", caption="git Commit Log") %>%
+            column_spec( 1,  bold = TRUE ) %>%
+            kable_styling("striped", full_width = TRUE)
+  )
+}
