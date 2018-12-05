da54f034ef748951b6dda7babb71891538a2ea16
avmaint <avmaint@uachome.org>
2018-12-05 07:31:13 -0500

    system updates

--- commonLightingKeypad.R
+++ commonLightingKeypad.R
@@ -6,7 +6,7 @@ get_keypad_buttons <- function() {
     ~Button, ~Name, ~B_Description,
     
     1, ".", "",  
-    2, "-", "",
+    2, "Rehearse", "For Rehearsal - Stage Pots and Sconce",
     
     3, "Ambience", "Use for walkin - intermission", 
     4, "Basic Event", "Front centre lit bright, house medium, plus accents",
@@ -14,7 +14,7 @@ get_keypad_buttons <- function() {
     5, "BSF 1", "Similar to Basic Event, plus Cross",
     6, "BSF 2", "Low levels for Video",
     
-    7, "On", "Maintenance: House and Stage Ketras on full white",
+    7, "Maintenance", "House and Stage Ketras on full white",
     8, "OFF", "Night Light Mode: Sconce only." 
   )
 return(keypad)
@@ -45,17 +45,19 @@ get_preset2zone <- function() {
     3,          13, "100% Blue",
     3,          8, "100% Magenta",
     3,          10, "100%",
+    3,          15, "75% Blue",
     
     4,           1, "75% Warm White",
     4,           5, "75% Warm White",
     4,           2, "80% Warm White",
     4,           4, "80% Warm White",
     4,           3, "80% Warm White",
-    4,          6, "80% Warm White",
+    4,          6, "70% Warm White",
     4,          7, "75% L.Pink",
     4,          12, "75% L.Pink",
     4,          13, "100% Blue",
     4,          8, "100% Pink",   
+    4,          15, "100% Blue",
     
     5,           1, "75% Warm White",
     5,           5, "75% Warm White",
@@ -73,11 +75,12 @@ get_preset2zone <- function() {
     6,           2, "30% Warm White",
     6,           4, "30% Warm White",
     6,           3, "30% Warm White",
-    6,          7, "75% L.Pink",
+    6,           7, "25% L.Pink",
     6,          12, "75% L.Pink",
     6,          13, "50% Blue",
-    6,          8, "50% Pink",
-    6,          10, "100%" 
+    6,           8,  "50% Pink",
+    6,          10, "100%" ,
+    6,           6,  "30%"
   )
 return(preset2zone)
 }
@@ -93,31 +96,32 @@ get_zones2dmx <- function() {
   plus_n <- function(nums, n) { 
     nums <- strsplit(nums,"[[:space:]]") %>% 
       unlist() %>% 
-       as.integer()
-      
-      as.character(nums + n) %>% paste(collapse=" ")  
+      as.integer() 
+    
+      as.character(nums + n) %>%
+      paste(collapse=" ")  
   }
   
   zones <- tribble(
     ~Zone, ~Z_Description,     ~Profile, ~DMX, 
-    1,  "Sconce",              "RGB",    "160" ,
-    2,  "House Pendants Wide", "RGB",    "163 166 169",
-    3,  "Upper Balcony",       "RGB",    "503",
+    1,  "Sconce",              "RGB",    "163",
+    2,  "House Pendants Wide", "RGB",    "503 506",
+    3,  "Upper Balcony",       "RGB",    "509 166",
     4,  "Under Balcony",       "RGB",    "500",
-    5,  "Stage pots",          "RGB",    "151 154 157",
-    6, "All Thrust",          "I",      "1 2 3 5 6 7",
-    7, "All Vocals",          "IRGBS",  "22 27 32 37 42 47",
-    8, "Floor Flares",        "IRGBS",  "172 177 182 187 192 197 202",
-    9, "Floor Bars",          "RGB",    "470", # not used?
-    10, "Cross",               "I",      "458", #wont work due to DMX path
+    5,  "Stage pots",          "RGB",    "151 154 157 160",
+    6, "All Thrust",             "I",    "1 2 3 9 10 11",
+    7, "All Vocals",         "IRGBS",    "22 27 32 37 42 47",
+    8, "Floor Flares",       "IRGBS",    "172 177 182 187 192 197 202",
+    9, "Floor Bars",           "RGB",    "470", # not used?
+    10, "Cross",                 "I",    "458", #wont work due to DMX path
     
-    12, "Wall Colour",         "RGBW",   t<-"219 231 243 255 267 279 291 303 315 327 339 351 363 375",
-    11, "Wall Intensity",      "I",       plus_n(t,9),
+    12, "Wall Colour",        "RGBW", t<-"219 231 243 255 267 279 291 303 315 327 339 351 363 375",
+    11, "Wall Intensity",        "I",     plus_n(t,9),
     
-    14, "Bulkhead Colour",     "RGBW",   "0 0 0 0", # not sure of the profile for bulkhead.
-    13, "Bulkhead Intensity",  "I",      "", #BH Colour +9",
+    14, "Bulkhead Colour",    "RGBW",    "0 0 0 0", # not sure of the profile for bulkhead.
+    13, "Bulkhead Intensity",    "I",    "", #BH Colour +9",
     
-    15, "Towers",              "RGBW",   "62 68 74 80"  #Amber and UV missed
+    15, "Towers",             "RGBW",    "62 68 74 80"  #Amber and UV missed
 
     ) %>% arrange(Zone)
   return(zones)
