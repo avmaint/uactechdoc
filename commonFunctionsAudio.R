4a441fb8572db1c1ac7fde4e66480fa47429a688
donert <terry.doner@gmail.com>
2026-01-29 10:04:27 -0500

    Updates for new audio

--- commonFunctionsAudio.R
+++ commonFunctionsAudio.R
@@ -1,66 +1,51 @@
 # Common code for SystemOperationsAudio and SystemDesignAudio 
 library(here)
+
 fname <- here("data", "uac_audio_config.xlsx")
+#fname <- "/Users/donert/Documents/UACTech/SystemDocumentation/github/uactechdoc/data/uac_audio_config.xlsx"
 
-work.config.consoleinputs <- read_excel(fname, 
-                                     sheet = "ConsoleInputs" )
+fnamedlive <- here('data', "uac_dlive_config.xlsx")
 
-
-
-work.config.dantepatch <- read_excel(fname, 
-                                       sheet = "DantePatch" )
-
-work.config.consoleoutputs <- read_excel(fname, 
-                                     sheet = "ConsoleOutputs" )
+db.sources <- read_excel(fname,  sheet = "Sources" )
+db.outputs <- read_excel(fname,  sheet = "Outputs" )
 
 get_monitors <- function() { 
   data <- tribble(
     ~Monitor, ~Name, ~Description,
     
-    1, "Vocal 1",   "Front vocal line - Left and Right",
-    2, "Vocal 2",   "Front vocal line - Centre",
-    
-    3, "Synth",     "Used for Synth", 
-    4, "Backline",  "Guitars and Bass",
-    
-    5, "Drums",     "Drums",
-    6, "Piano",      "Piano"
-    
+    1, "Vocal C",    "Front Centre",
+    2, "Vocal LR",   "Front Left and Right",
+    3, "Sz",         "Synthesizer", 
+    4, "Pn",         "Piano",
+    5, "Backline",   "Guitars and Bass",
+    6, "Drums",      "Drums",
+    7, "Unassigned", "Unassigned",
+    8, "Unassigned", "Unassigned"
   )
   return(data)
 }
 
-#TODO - Hardcoded assumption about scene 002
+get_inputs <- function( device ) {
 
-m7in.002 <- work.config.consoleinputs %>%
-             filter(Device == "ZAKU-0001") %>% select(-Device)
-
-dmin.002 <- work.config.consoleinputs %>%
-             filter(Device == "ZAKU-0002") %>% select(-Device)
-
-get_inputs <- function(console, scene) {
-  if (scene == "002") {
-    if (console == "M7CL") 
-      {res <- m7in.002 }
-    else if (console == "DM1K") 
-      {res <- dmin.002} 
-    else 
-      stop(paste("Unknown console:", console))
-    }
-  else 
-      stop(paste("Unknown Scene:", scene))
-  
+	if ( ! (device   %in% db.sources$Device ) )
+		stop(paste("Input Edit Error: Unknown device-", device))
+		
+  res <- db.sources |>
+		filter(Device == device) |>
+  	    filter(!is.na(Name))
+  			 	
   return(res)
 }
 
-get_dante_outputs <- function() {
-	data <- read_excel(fname
-					   , sheet="dante_outputs")
-	return(data)
-}
+get_outputs <- function( device ) {
 
-get_dante_devices <- function() {
-	db <- read_excel(fname, sheet = "DanteDevices" )
-	return(db)
+	if ( ! (device   %in% db.outputs$Device ) )
+		stop(paste("Output Edit Error: Unknown device-", device))
+		
+  res <- db.outputs |>
+		filter(Device == device) |>
+  	    filter(!is.na(Name))
+  			 	
+  return(res)
 }
 	
